a0e2721e9a69b95925c0e644f2a97600fdee1120
Hao Feng <fh1995@mail.ustc.edu.cn>
2024-05-08 16:13:33 +0800

    Update eval.m

--- eval.m
+++ eval.m
@@ -16,7 +16,8 @@ for i=1:65
     path_rec_1 = sprintf("%s%d%s", path_rec, i, '_1 copy_rec.png');  % rectified image path
     path_rec_2 = sprintf("%s%d%s", path_rec, i, '_2 copy_rec.png');  % rectified image path
     path_scan_new = sprintf("%s%d%s", path_scan, i, '.png');  % corresponding scan image path
-
+    bbox_i_path = sprintf("%s%d%s", label_path, i, '.txt');   % corresponding layout txt path
+    
     % imread and rgb2gray
     A1 = imread(path_rec_1);
     A2 = imread(path_rec_2);
@@ -30,16 +31,21 @@ for i=1:65
     A1 = rgb2gray(A1);
     A2 = rgb2gray(A2);
     ref = rgb2gray(ref);
+    bbox_i = read_txt(bbox_i_path);
+    bbox_i = bbox_i + 1; % python index starts from 0
 
     % resize
     b = sqrt(tarea/size(ref,1)/size(ref,2));
     ref = imresize(ref,b);
     A1 = imresize(A1,[size(ref,1),size(ref,2)]);
     A2 = imresize(A2,[size(ref,1),size(ref,2)]);
+    scaled_bbox_i = bbox_i * b * 0.5;
+    scaled_bbox_i = round(scaled_bbox_i);
+    scaled_bbox_i = max(scaled_bbox_i, 1);
 
     % calculate
-    [ms_1, ld_1, lid_1, W_v_1, W_h_1] = evalUnwarp(A1,ref);
-    [ms_2, ld_2, lid_2, W_v_2, W_h_2] = evalUnwarp(A2,ref);
+    [ms_1, ld_1, lid_1, W_v_1, W_h_1] = evalUnwarp(A1, ref, scaled_bbox_i);
+    [ms_2, ld_2, lid_2, W_v_2, W_h_2] = evalUnwarp(A2, ref, scaled_bbox_i);
     ms1 = ms1 + ms_1;
     ms2 = ms2 + ms_2;
     ld1 = ld1 + ld_1;
@@ -47,7 +53,7 @@ for i=1:65
     lid1 = lid1 + lid_1;
     lid2 = lid2 + lid_2;
     wv = wv + W_v_1 + W_v_2;
-    wh = wv + W_h_1 + W_h_2;
+    wh = wh + W_h_1 + W_h_2;
 end
 
 ms = (ms1 + ms2) / 130  % MS-SSIM
