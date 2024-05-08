f637d2b005ccecd4a4d6811524a168337ac21b77
Hao Feng <fh1995@mail.ustc.edu.cn>
2024-05-08 16:07:58 +0800

    Update evalUnwarp.m

--- evalUnwarp.m
+++ evalUnwarp.m
@@ -1,4 +1,4 @@
-function [ms, ld, std_mean, d_floor, lid_dist] = evalUnwarp(A, ref)
+function [ms, ld, li_d, wv, wh] = evalUnwarp(A, ref)
 %EVALUNWARP compute MSSSIM and LD between the unwarped image and the scan
 %   A:      unwarped image
 %   ref:    reference image, the scan image
@@ -45,21 +45,14 @@ SIFTflowpara.nIterations= 30;
 
 [vx,vy,~]=SIFTflowc2f(sift1,sift2,SIFTflowpara);
 
-
-% LD
 rows1p = size(im1,1);
 cols1p = size(im1,2);
 
-% plot 2
-lid_dist = zeros(1,100);
-
+% Li-D
 rowstd_sum = 0;
 for i = 1:rows1p
     rowstd = std(vy(i, :),1); 
     rowstd_sum = rowstd_sum + rowstd;
-    if rowstd < 10
-        lid_dist(floor(rowstd*10)+1) = lid_dist(floor(rowstd*10)+1) + 1;
-    end
 end
 rowstd_mean = rowstd_sum / rows1p;
 
@@ -67,21 +60,15 @@ colstd_sum = 0;
 for i = 1:cols1p
     colstd = std(vx(:, i),1); 
     colstd_sum = colstd_sum + colstd;
-    if colstd < 10
-        lid_dist(floor(colstd*10)+1) = lid_dist(floor(colstd*10)+1) + 1;
-    end
 end 
 colstd_mean = colstd_sum / cols1p;
     
-std_mean = (rowstd_mean + colstd_mean) / 2;
+li_d = (rowstd_mean + colstd_mean) / 2;
 
 
-% Li-D
+% LD
 d = sqrt(vx.^2 + vy.^2);
 ld = mean(d(:));
-
-% plot 1
-d_floor = floor(d);
 
 
 % MS-SSIM
@@ -93,5 +80,23 @@ for s = 1 : 5
     y = impyramid(y, 'reduce');
 end
 ms = wt * ss;
+
+
+% wv and wh
+rowstd_sum = 0;
+for i = 1:size(data, 1)
+    rowstd_top = std(vy(data(i,2), data(i,1):data(i,3)),1) / (data(i,3)-data(i,1)); 
+    rowstd_bot = std(vy(data(i,4), data(i,1):data(i,3)),1) / (data(i,3)-data(i,1)); 
+    rowstd_sum = rowstd_sum + rowstd_top + rowstd_bot;
+end
+wv = rowstd_sum / (2 * size(data, 1));
+
+colstd_sum = 0;
+for i = 1:size(data, 1)
+    colstd_left = std(vx(data(i,2):data(i,4), data(i,1)),1) / (data(i,4)- data(i,2)); 
+    colstd_right = std(vx(data(i,2):data(i,4), data(i,3)),1) / (data(i,4)- data(i,2)); 
+    colstd_sum = colstd_sum + colstd_left + colstd_right;
+end 
+wh = colstd_sum / (2 * size(data, 1));
 
 end
